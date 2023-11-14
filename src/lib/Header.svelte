9b45a5fe0e205c1f4f7f61c6a9482051a1ed0308
jesson-stylesheet <145638928+jesson-stylesheet@users.noreply.github.com>
2023-11-14 22:53:25 +0800

    Finishing up the main layout

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,22 +1,30 @@
 <script>
     // Import Material Web Components
-    import '@material/mwc-button';
-    import '@material/mwc-icon';
+    import '@material/web/button/filled-button.js';
+    import '@material/web/icon/icon.js';
+    import '@material/web/iconbutton/filled-icon-button.js';
   </script>
-  
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
   <header>
     <div class="header-content">
       <h1>My Website</h1>
       <nav>
-        <mwc-button icon="home" label="Home"></mwc-button>
-        <mwc-button icon="info" label="About"></mwc-button>
-        <mwc-button icon="mail" label="Contact"></mwc-button>
+        <md-filled-button label="Home"></md-filled-button>
+        <md-filled-button label="About"></md-filled-button>
+        <md-filled-button label="Contact"></md-filled-button>
+        <md-icon-button>
+          <span class="material-symbols-outlined">
+            menu
+            </span>
+        </md-icon-button>
       </nav>
     </div>
   </header>
   
   <style>
     header {
+      width: 80%;
+      margin-left: 20%;
       background-color: #6200ee; /* Example color */
       color: white;
       padding: 1rem;
@@ -30,7 +38,7 @@
       font-size: 1.5rem;
     }
   
-    nav mwc-button {
+    nav md-filled-button {
       margin: 0 0.5rem;
     }
   </style>  
--- src/lib/SideMenu.svelte
+++ src/lib/SideMenu.svelte
@@ -1,73 +1,63 @@
-<script lang="ts">
-  import '@material/mwc-list/mwc-list';
-  import '@material/mwc-list/mwc-list-item';
+<script>
+  import { onMount, onDestroy } from 'svelte';
+  import '@material/web/list/list.js';
+  import '@material/web/list/list-item.js';
 
-  let isOpen = false;
-
-  // Reactive statement to handle clicks outside the menu
-  $: if (isOpen) {
-    window.addEventListener('click', handleClickOutside);
-  } else {
-    window.removeEventListener('click', handleClickOutside);
-  }
+  let isOpen = true;
 
   function toggleMenu() {
     isOpen = !isOpen;
   }
 
-  function handleClickOutside(event: MouseEvent) {
-    // Using type assertion to ensure event.target is of type Element
-    const target = event.target as Element;
+  /*function handleClickOutside(event) {
+    const target = event.target;
     if (!target.closest('.side-menu')) {
       isOpen = false;
     }
   }
+
+  onMount(() => {
+    window.addEventListener('click', handleClickOutside);
+
+    return () => {
+      window.removeEventListener('click', handleClickOutside);
+    };
+  });*/
 </script>
 
 <div class="side-menu" class:open={isOpen}>
-  <mwc-list>
-    <mwc-list-item>Home</mwc-list-item>
-    <mwc-list-item>About</mwc-list-item>
-    <mwc-list-item>Services</mwc-list-item>
-    <mwc-list-item>Contact</mwc-list-item>
-  </mwc-list>
+  <md-list>
+    <md-list-item>Home</md-list-item>
+    <md-list-item>About</md-list-item>
+    <md-list-item>Services</md-list-item>
+    <md-list-item>Contact</md-list-item>
+  </md-list>
 </div>
 
-<button class="menu-toggle" on:click={toggleMenu}>Menu</button>
-  
-  <div class="side-menu" class:open={isOpen}>
-    <mwc-list>
-      <mwc-list-item>Home</mwc-list-item>
-      <mwc-list-item>About</mwc-list-item>
-      <mwc-list-item>Services</mwc-list-item>
-      <mwc-list-item>Contact</mwc-list-item>
-    </mwc-list>
-  </div>
-  
-  <button class="menu-toggle" on:click={toggleMenu}>Menu</button>
-  
-  <style>
-    .side-menu {
-      width: 250px;
-      background-color: #fff;
-      box-shadow: 3px 0 6px rgba(0, 0, 0, 0.16);
-      position: fixed;
-      top: 0;
-      bottom: 0;
-      left: -250px;
-      transition: left 0.3s ease;
-      z-index: 100;
-    }
-  
-    .side-menu.open {
-      left: 0;
-    }
-  
-    .menu-toggle {
-      position: fixed;
-      top: 10px;
-      left: 10px;
-      z-index: 101;
-    }
-  </style>
-  
+<!-- <button class="menu-toggle" on:click={toggleMenu}>Menu</button> -->
+
+<style>
+  .side-menu {
+    width: 20%;
+    max-width: 250px;
+    background-color: #fff;
+    box-shadow: 3px 0 6px rgba(0, 0, 0, 0.16);
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: -250px;
+    transition: left 0.3s ease;
+    z-index: 100;
+  }
+
+  .side-menu.open {
+    left: 0;
+  }
+
+  /*.menu-toggle {
+    position: fixed;
+    top: 10px;
+    left: 10px;
+    z-index: 101;
+  } */
+</style>
